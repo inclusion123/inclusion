25bcbbf5f4e626ab6368a64d287a4d1c78418be1
dev <dev@example.com>
2022-09-20 14:07:50 +0530

    team page changes

--- resources/views/front/pages/team/_partials/team_member_page.blade.php
+++ resources/views/front/pages/team/_partials/team_member_page.blade.php
@@ -17,10 +17,11 @@
                     <div class="col-lg-3 col-md-4 col-sm-6 col-12 wow slideInUp" data-wow-delay="0.3s">
                         {{-- <div class="team-item bg-light rounded overflow-hidden"> --}}
                         <div class="team-item rounded overflow-hidden">    
+                            {{-- <div class="team-img position-relative overflow-hidden blinking"> --}}
                             <div class="team-img position-relative overflow-hidden">
                                 <img class="img-fluid w-100" src="{{ asset('storage/images') }}/{{$team_member->image}}"
                                     alt="" />
-                                <div class="team-social">
+                                {{-- <div class="team-social">
                                     <a class="btn btn-lg btn-primary btn-lg-square" href="{{$team_member->twitter}}"><i
                                             class="fab fa-twitter fw-normal"></i></a>
                                     <a class="btn btn-lg btn-primary btn-lg-square" href="{{$team_member->facebook}}"><i
@@ -29,7 +30,7 @@
                                             class="fab fa-instagram fw-normal"></i></a>
                                     <a class="btn btn-lg btn-primary btn-lg-square" href="{{$team_member->linkedin}}"><i
                                             class="fab fa-linkedin-in fw-normal"></i></a>
-                                </div>
+                                </div> --}}
                             </div>
                             <div class="text-center py-4">
                                 <h4 class="text-primary">{{$team_member->name}}</h4>
