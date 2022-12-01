37dd74dab1cbe413cb932cc0d64aa3f5e33098cb
tdev <tdev@gmail.com>
2022-12-01 15:20:56 +0530

    christmas offer section

--- resources/views/front/layouts/topbar.blade.php
+++ resources/views/front/layouts/topbar.blade.php
@@ -4,7 +4,7 @@
         <div class="col-lg-12 text-center text-lg-start mb-2 mb-lg-0">
             <div class="alert alert_black_friday alert-success alert-dismissible">
                 <button type="button" class="btn-close" data-bs-dismiss="alert">x</button>
-                Get <strong>30% Off</strong> On Websites Development
+                Get <strong>40% Off</strong> On Websites Development & Maintenance Service
             </div>
         </div>
     </div>
