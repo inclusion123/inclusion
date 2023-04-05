198a1c19a1ccad953788ea3502f29fc107f01493
Akshay <akshay_kumar@inclusionsoft.com>
2023-04-05 11:09:19 +0530

    remove 40% off message from topbar

--- resources/views/front/layouts/topbar.blade.php
+++ resources/views/front/layouts/topbar.blade.php
@@ -2,10 +2,10 @@
 <div class="black_friday_header container-fluid px-5 d-none d-lg-block">
     <div class="row gx-0">
         <div class="col-lg-12 text-center text-lg-start mb-2 mb-lg-0">
-            <div class="alert alert_black_friday alert-success alert-dismissible">
+            {{-- <div class="alert alert_black_friday alert-success alert-dismissible">
                 <button type="button" class="btn-close" data-bs-dismiss="alert">x</button>
                 Get <strong>40% Off</strong> On Websites Development & Maintenance Services
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
