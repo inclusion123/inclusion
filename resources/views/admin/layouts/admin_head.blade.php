a0b8d9e711103aaeeadaab3b1a50ad928edf224c
dev <dev@example.com>
2023-04-27 13:12:08 +0530

    add favicon

--- resources/views/admin/layouts/admin_head.blade.php
+++ resources/views/admin/layouts/admin_head.blade.php
@@ -1,3 +1,6 @@
+      <!-- Favicon -->
+      <link href="{{ asset('favicon.png')}}" rel="icon" />
+
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
